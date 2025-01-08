e57bebf1a157b884a7575dbc6003c87f6059dbcb
okmic <okmic.g@gmail.com>
2025-01-08 22:05:44 +0300

    words for identify users

--- src/modules/admin_telegram/helper.ts
+++ src/modules/admin_telegram/helper.ts
@@ -3,56 +3,75 @@ import { KeyContentUpdateWord } from "./types";
 class TelegramAdminHelper {
 
     keysUpdateWords: KeyContentUpdateWord[] = [
-        { word: "обновить", key: "someUpdate" },
-        { word: "обнови", key: "someUpdate" },
-        { word: "update", key: "someUpdate" },
-        { word: "email", key: "email" },
-        { word: "почта", key: "email" },
-        { word: "почту", key: "email" },
-        { word: "телефон", key: "phone" },
-        { word: "phone", key: "phone" },
-
-        { word: "продукт", key: "products" },
-        { word: "продукты", key: "products" },
-        { word: "товары", key: "products" },
-        { word: "листинг", key: "products" },
-        { word: "products", key: "products" },
-        { word: "product", key: "products" },
-
-        { word: "Название компании", key: "logoName" },
-        { word: "Главный заголовок", key: "logoName" },
-
-        { word: "Логотип", key: "logoImgUrl" },
-        { word: "logo", key: "logoImgUrl" },
-        { word: "Главная изображение", key: "logoImgUrl" },
-        { word: "Главная картинка", key: "logoImgUrl" },
-
-        { word: "Описание компании", key: "companyDescription" },
-        { word: "Описание", key: "companyDescription" },
-        { word: "decsr", key: "companyDescription" },
-        { word: "decsription", key: "companyDescription" },
-
-        { word: "адрес", key: "address" },
-        { word: "address", key: "address" },
-
-
-        { word: "Заголовок главного блока", key: "mainBlockTitle" },
-        { word: "Описание главного", key: "mainBlockDescription" },
-
-        { word: "о работе", key: "works" },
-        { word: "works", key: "works" },
-        { word: "work", key: "works" },
-
-        { word: "услуги", key: "services" },
-        { word: "service", key: "services" },
-        { word: "services", key: "services" },
-
-        { word: "cоц сети", key: "socialMedia" },
-        { word: "сети", key: "socialMedia" },
-        { word: "соц", key: "socialMedia" },
-        { word: "social", key: "socialMedia" },
-        { word: "social media", key: "socialMedia" },
+        { "word": "обновить", "key": "someUpdate" },
+        { "word": "обнови", "key": "someUpdate" },
+        { "word": "update", "key": "someUpdate" },
+        { "word": "email", "key": "email" },
+        { "word": "почта", "key": "email" },
+        { "word": "почту", "key": "email" },
+        { "word": "телефон", "key": "phone" },
+        { "word": "phone", "key": "phone" },
+    
+        { "word": "продукт", "key": "products" },
+        { "word": "продукты", "key": "products" },
+        { "word": "товары", "key": "products" },
+        { "word": "листинг", "key": "products" },
+        { "word": "products", "key": "products" },
+        { "word": "product", "key": "products" },
+    
+        { "word": "Название компании", "key": "logoName" },
+        { "word": "Главный заголовок", "key": "logoName" },
+    
+        { "word": "Логотип", "key": "logoImgUrl" },
+        { "word": "logo", "key": "logoImgUrl" },
+        { "word": "Главная изображение", "key": "logoImgUrl" },
+        { "word": "Главная картинка", "key": "logoImgUrl" },
+    
+        { "word": "Описание компании", "key": "companyDescription" },
+        { "word": "Описание", "key": "companyDescription" },
+        { "word": "decsr", "key": "companyDescription" },
+        { "word": "decsription", "key": "companyDescription" },
+    
+        { "word": "адрес", "key": "address" },
+        { "word": "address", "key": "address" },
+    
+        { "word": "Заголовок главного блока", "key": "mainBlockTitle" },
+        { "word": "Описание главного", "key": "mainBlockDescription" },
+    
+        { "word": "о работе", "key": "works" },
+        { "word": "works", "key": "works" },
+        { "word": "work", "key": "works" },
+    
+        { "word": "услуги", "key": "services" },
+        { "word": "service", "key": "services" },
+        { "word": "services", "key": "services" },
+    
+        { "word": "cоц сети", "key": "socialMedia" },
+        { "word": "сети", "key": "socialMedia" },
+        { "word": "соц", "key": "socialMedia" },
+        { "word": "social", "key": "socialMedia" },
+        { "word": "social media", "key": "socialMedia" },
+    
+        { "word": "помощь", "key": "someUpdate" },
+        { "word": "вопрос", "key": "someUpdate" },
+        { "word": "поддержка", "key": "someUpdate" },
+        { "word": "связаться", "key": "someUpdate" },
+        { "word": "связь", "key": "someUpdate" },
+        { "word": "информация", "key": "someUpdate" },
+        { "word": "запрос", "key": "someUpdate" },
+        { "word": "отзыв", "key": "someUpdate" },
+        { "word": "проблема", "key": "someUpdate" },
+        { "word": "заказ", "key": "someUpdate" },
+        { "word": "состояние заказа", "key": "someUpdate" },
+        { "word": "доставка", "key": "someUpdate" },
+        { "word": "возврат", "key": "someUpdate" },
+        { "word": "оплата", "key": "someUpdate" },
+        { "word": "счет", "key": "someUpdate" },
+        { "word": "подписка", "key": "someUpdate" },
+        { "word": "акция", "key": "someUpdate" },
+        { "word": "скидка", "key": "someUpdate" }
     ]
+    
 }
 
 export default new TelegramAdminHelper()
